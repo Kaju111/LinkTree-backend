033154bbcf5f728a4a44ee4d68bbd7dd7c020d76
kaju111 <saikiakaju275@gmail.com>
2023-09-29 12:42:12 +0530

    first commit

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,7 +1,20 @@
 const User = require('../models/user')
+const jwt = require('jsonwebtoken')
 
-const registerUser =  (req, res) =>{
-    res.send(`register from callback`)
+const registerUser = async (req, res) =>{
+    const { handle, email, password, category } =req.body;
+    console.log(req.body)
+    try {
+        const defaultLink = {url: 'typefinance.com', title: 'TypeFinance.com', icon: ''}
+        const user = await User.create({handle, email, password, role: category, links: [defaultLink]})
+        const token = jwt.sign({email: email},process.evn.SECRET_JWT)
+        return res.json({message: 'user created', status: 'success', 'token':token, id: user._id}) 
+    } catch (error) {
+        if(error.code === '11000'){
+            return res.json({message: 'Try different handle or email', status: 'error'})
+        }
+        return res.json({message: error.message, status: 'error',}) 
+    }  
 }
 
 const loginUser = (req, res) =>{
--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
 const mongoose = require('mongoose')
 mongoose.set('strictQuery', false)
 const {registerUser, loginUser} = require('./controllers/auth')
+require('dotenv').config()
 
 
-mongoose.connect('mongodb://127.0.0.1:27017/linkTree-9').then(() => {console.log(`mongodb Connected`)}).catch(err=>{console.log(err.message)})
+app.use(cors())
+app.use(express.json())
+
+mongoose.connect('mongodb://127.0.0.1:27017/linkTree-9').then(() => {console.log(`mongodb COnnected`)}).catch(err=>{console.log(err.message)})
 
 app.get('/', (req, res) =>{
     res.send(`Server is running on port ${port}`)
